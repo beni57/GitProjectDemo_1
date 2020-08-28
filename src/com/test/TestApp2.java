e5a01c1c51afabfac95802824b54b542d3166204
behnaz78 <behnaz.vatan@gmail.com>
2020-08-28 12:58:42 -0700

    commit file!!

---
+++ src/com/test/TestApp2.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class TestApp2 {
+
+	public static void main(String[] args) {
+
+		String str = "hungry";
+		
+		if( str == "hungry") {
+			System.out.println(" eat somethings!!!");
+		}else {
+			System.out.println(" nothing !!!!");
+		}
+	}
+
+}
