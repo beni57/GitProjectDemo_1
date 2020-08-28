de60239cb897752bbcffa4dc00c83f161eefb6d5
behnaz78 <behnaz.vatan@gmail.com>
2020-08-28 10:03:55 -0700

    First Commit!!

---
+++ src/com/test/TestApp.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+
+		String str = "hungry";
+		
+		if( str == "hungry") {
+			System.out.println(" eat somethings!!!");
+		}else {
+			System.out.println(" nothing !!!!");
+		}
+	}
+
+}
